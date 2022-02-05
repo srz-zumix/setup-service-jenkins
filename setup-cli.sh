f2aac1446c55ee42c1b65d4afab0ab9c0a5b94a2
srz_zumix <zumix.cpp@gmail.com>
2022-02-05 20:34:15 +0900

    up

--- setup-cli.sh
+++ setup-cli.sh
@@ -16,7 +16,7 @@ echo "${PREFIX}" >>"${GITHUB_PATH}"
 
 cp "${GITHUB_ACTION_PATH}/resources/jenkins-cli.in" "${PREFIX}/jenkins-cli"
 sed -i "s#@jenkins_url@#${JENKINS_URL}#g" "${PREFIX}/jenkins-cli"
-sed -i "s#@jenkins_cli_jar@#${PREFIX}#g" "${PREFIX}/jenkins-cli"
+sed -i "s#@jenkins_cli_jar@#${PREFIX}/jenkins-cli.jar#g" "${PREFIX}/jenkins-cli"
 chmod +x "${PREFIX}/jenkins-cli"
 
 "${PREFIX}/jenkins-cli" help
