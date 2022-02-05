d8889f7fb4478cc732e88ec0573bdbe06121eb48
srz_zumix <zumix.cpp@gmail.com>
2022-02-05 20:41:20 +0900

    up

--- setup-cli.sh
+++ setup-cli.sh
@@ -19,4 +19,6 @@ sed -i "s#@jenkins_url@#${JENKINS_URL}#g" "${PREFIX}/jenkins-cli"
 sed -i "s#@jenkins_cli_jar@#${PREFIX}/jenkins-cli.jar#g" "${PREFIX}/jenkins-cli"
 chmod +x "${PREFIX}/jenkins-cli"
 
+echo '::group::jenkins-cli help'
 "${PREFIX}/jenkins-cli" help
+echo '::endgroup::'
