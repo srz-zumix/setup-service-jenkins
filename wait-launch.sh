0101ffdbff7635631241fddf55a4704f7bab93bc
srz_zumix <zumix.cpp@gmail.com>
2022-02-06 21:49:11 +0900

    fix log

--- wait-launch.sh
+++ wait-launch.sh
@@ -4,5 +4,5 @@ until docker logs "$1" 2>&1 | grep "Jenkins is fully up and running" >/dev/null;
   sleep 30; echo "waiting jenkins launch..."
 done
 echo '::group::jenkins docker log'
-docker logs "$1"
+docker logs "$1" 2>&1
 echo '::endgroup::'
