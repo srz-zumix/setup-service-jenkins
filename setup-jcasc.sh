6fe5e44070a16bc1593a4f617983882d911b029a
srz_zumix <zumix.cpp@gmail.com>
2022-04-25 21:29:08 +0900

    log

--- setup-jcasc.sh
+++ setup-jcasc.sh
@@ -3,11 +3,14 @@
 SERVICE_ID=$1
 SERVICE_JCASC_PATH=$(jenkins-cli-groovy 'println System.getProperty("casc.jenkins.config")')
 
+echo '::group::copy jcasc'
 if [ -d "${JCASC_PATH}" ]; then
     docker cp "${JCASC_PATH}/." "${SERVICE_ID}:${SERVICE_JCASC_PATH}"
 else
     docker cp "${JCASC_PATH}"   "${SERVICE_ID}:${SERVICE_JCASC_PATH}"
 fi
+docker exec "${SERVICE_ID}" ls "${SERVICE_JCASC_PATH}"
+echo '::endgroup::'
 
 echo '::group::jenkins-cli reload-configuration'
 jenkins-cli reload-configuration
