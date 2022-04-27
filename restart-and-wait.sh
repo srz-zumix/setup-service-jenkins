dc6e1a035bae022c99192284bcb70215f9c924c1
srz_zumix <zumix.cpp@gmail.com>
2022-04-28 08:43:18 +0900

    fix wait

--- restart-and-wait.sh
+++ restart-and-wait.sh
@@ -5,11 +5,7 @@ echo '::group::jenkins restart'
 # get session id
 PREV_ID=$(jenkins-cli session-id)
 
-while jenkins-cli session-id | grep "${PREV_ID}"; do
-    # restart
-    echo "restart request..."
-    jenkins-cli restart
-
+wait() {
     local -i attempt_num=1
     until jenkins-cli session-id 2>/dev/null; do
         sleep 30; echo "waiting jenkins response..."
@@ -19,6 +15,15 @@ while jenkins-cli session-id | grep "${PREV_ID}"; do
         fi
         let attempt_num++
     done
+}
+
+while jenkins-cli session-id | grep "${PREV_ID}"; do
+    # restart
+    echo "restart request..."
+    jenkins-cli restart
+
+    wait
+
     echo "checking session-id..."
 done
 
