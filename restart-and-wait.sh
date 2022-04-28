3d6ef80ac489b41a73ea1bbabb7b9a116aed385b
srz_zumix <zumix.cpp@gmail.com>
2022-04-28 09:10:24 +0900

    update restart and wait

--- restart-and-wait.sh
+++ restart-and-wait.sh
@@ -6,15 +6,18 @@ echo '::group::jenkins restart'
 PREV_ID=$(jenkins-cli session-id)
 
 wait() {
+    sleep 5
+
     local attempt_max=4
     local -i attempt_num=1
     until jenkins-cli session-id 2>/dev/null; do
         sleep 30; echo "waiting jenkins response..."
-        if ((attempt_num == attempt_max)); then
+        if ((attempt_num == 1)); then
             docker logs "${JENKINS_SERVICE_ID}"
+            echo 'container restart'
             docker container restart "${JENKINS_SERVICE_ID}"
         else
-            if ((attempt_num == attempt_max*2)); then
+            if ((attempt_num == attempt_max)); then
                 docker logs "${JENKINS_SERVICE_ID}"
                 exit 1
             fi
@@ -29,8 +32,6 @@ while jenkins-cli session-id | grep "${PREV_ID}"; do
     jenkins-cli restart
 
     wait
-
-    echo "checking session-id..."
 done
 
 echo '::endgroup::'
