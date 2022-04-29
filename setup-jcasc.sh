200f24f96f5e8ba533ad63939ee97445ced7d904
srz_zumix <zumix.cpp@gmail.com>
2022-04-29 15:57:46 +0900

    up

--- setup-jcasc.sh
+++ setup-jcasc.sh
@@ -12,6 +12,7 @@ SERVICE_JCASC_PATH="${SERVICE_JCASC_PATH_JAVAOPT:-${SERVICE_JCASC_PATH_ENV}}"
 
 if [ -z "${SERVICE_JCASC_PATH}" ]; then
   # ${JENKINS_HOME}/jenkins.yml is jcasc default path
+  jenkins-cli-groovy "casc = GlobalConfiguration.all().get(CasCGlobalConfig.class); cascPath = casc != null ? casc.getConfigurationPath() : null; println(cascPath)"
   JENKINS_HOME=$(docker exec "${JENKINS_SERVICE_ID}" echo '${JENKINS_HOME}')
   SERVICE_JCASC_PATH="${JENKINS_HOME}/jenkins.yml/"
 fi
@@ -49,5 +50,5 @@ fi
 
 # dump
 echo '::group::jenkins dump jcasc'
-jenkins-cli-groovy "out = new ByteArrayOutputStream(); io.jenkins.plugins.casc.ConfigurationAsCode.get().export(out); println(out.toString())"
+jenkins-cli-groovy "out = new ByteArrayOutputStream(); io.jenkins.plugins.casc.ConfigurationAsCode.get().export(out); out.flush(); println(out.toString())"
 echo '::endgroup::'
