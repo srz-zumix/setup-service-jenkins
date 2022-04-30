36acdf1d7eef4e6d9e8c5e6eff9767c7b8e20838
srz_zumix <zumix.cpp@gmail.com>
2022-04-30 19:53:17 +0900

    up

--- setup-initial.sh
+++ setup-initial.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+TEMP="${RUNNER_TEMP}"
+if [ -z "${TEMP}" ]; then
+  TEMP="$(mktemp -d)"
+fi
+
+PREFIX="${TEMP}/jenkins/bin"
+
+mkdir -p "${PREFIX}"
+echo "${PREFIX}" >>"${GITHUB_PATH}"
+
 # docker log
 sed -e "s#@container_id@#${JENKINS_SERVICE_ID}#g" \
     "${GITHUB_ACTION_PATH}/resources/jenkins-log.in" \
