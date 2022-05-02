a98cb9528e21ce9f5c4dc3cd79c9cb720de8224e
srz_zumix <zumix.cpp@gmail.com>
2022-05-02 14:37:15 +0900

    up

--- setup-initial.sh
+++ setup-initial.sh
@@ -16,10 +16,18 @@ sed -e "s#@container_id@#${JENKINS_SERVICE_ID}#g" \
     > "${PREFIX}/jenkins-log"
 chmod +x "${PREFIX}/jenkins-log"
 
-docker inspect "${JENKINS_SERVICE_ID}"
+INSPECT_ENVS=$(docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' "${JENKINS_SERVICE_ID}")
+echo "${INSPECT_ENVS}"
 
-# coppy logging.properties
-docker cp "${GITHUB_ACTION_PATH}/resources/logging.properties" "${JENKINS_SERVICE_ID}:/var/jenkins_home/logging.properties"
+JENKINS_JAVA_OPTS=$(echo ${INSPECT_ENVS} | grep JAVA_OPTS= | cu -d'=' -f2)
+echo "${JENKINS_JAVA_OPTS}"
+
+if [ "${JENKINS_JAVA_OPTS}" =~ "java.util.logging.config.file" ]; then
+  LOGGING_PROPERTIES_FILE=$(echo "${JENKINS_JAVA_OPTS}" | sed 's/-Djava.util.logging.config.file=[\S]*/\1/g')
+  echo ${LOGGING_PROPERTIES_FILE}
+  # coppy logging.properties
+  docker cp "${GITHUB_ACTION_PATH}/resources/logging.properties" "${JENKINS_SERVICE_ID}:${LOGGING_PROPERTIES_FILE]"
+fi
 
 # restart
 "${GITHUB_ACTION_PATH}/restart-and-wait.sh"
