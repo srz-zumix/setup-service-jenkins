c81b09040bbda895800796ea841834087152e8e3
srz_zumix <zumix.cpp@gmail.com>
2022-05-02 20:06:27 +0900

    up

--- setup-initial.sh
+++ setup-initial.sh
@@ -16,18 +16,25 @@ sed -e "s#@container_id@#${JENKINS_SERVICE_ID}#g" \
     > "${PREFIX}/jenkins-log"
 chmod +x "${PREFIX}/jenkins-log"
 
+echo '::group::jenkins initialize for JAVA_OPT'
+
 INSPECT_ENVS=$(docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' "${JENKINS_SERVICE_ID}")
 echo "${INSPECT_ENVS}"
 
 JENKINS_JAVA_OPTS=$(echo ${INSPECT_ENVS} | grep JAVA_OPTS= | cut -d'=' -f2-)
 echo "${JENKINS_JAVA_OPTS}"
 
-if [[ "${JENKINS_JAVA_OPTS}" =~ java.util.logging.config.file ]]; then
-  LOGGING_PROPERTIES_FILE=$(echo "${JENKINS_JAVA_OPTS}" | sed 's/.*-Djava.util.logging.config.file=([\S]*).*/\1/g')
-  echo "${LOGGING_PROPERTIES_FILE}"
-  # coppy logging.properties
-  docker cp "${GITHUB_ACTION_PATH}/resources/logging.properties" "${JENKINS_SERVICE_ID}:${LOGGING_PROPERTIES_FILE}"
-fi
+for opt_set in $(echo "${JENKINS_JAVA_OPTS}" | cut -d' '); then
+  OPT=$(echo "${opt_set}" | cut -d'=' -f1)
+  VAL=$(echo "${opt_set}" | cut -d'=' -f2-)
+  echo "${OPT}: ${VAL}"
+  if [ "${OPT}" == "java.util.logging.config.file" ]; then
+    echo "coppy logging properties" 
+    docker cp "${GITHUB_ACTION_PATH}/resources/logging.properties" "${JENKINS_SERVICE_ID}:${VAL}"
+  fi
+done
+
+echo '::endgroup::'
 
 # restart
 "${GITHUB_ACTION_PATH}/restart-and-wait.sh"
