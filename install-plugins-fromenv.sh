aed906010e540ff30bf7a224faf4532414063f59
srz_zumix <zumix.cpp@gmail.com>
2022-05-18 07:20:10 +0900

    reduce restart

--- install-plugins-fromenv.sh
+++ install-plugins-fromenv.sh
@@ -4,7 +4,4 @@ if [ -n "${INSTALL_PLUGINS}" ]; then
     echo '::group::jenkins plugin install'
     echo "${INSTALL_PLUGINS}" | xargs -I{} jenkins-cli install-plugin {}
     echo '::endgroup::'
-
-    # restart
-    "${GITHUB_ACTION_PATH}/restart-and-wait.sh"
 fi
