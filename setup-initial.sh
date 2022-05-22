762168df9d7066cdd81a8d2d06800f711606f14a
srz_zumix <zumix.cpp@gmail.com>
2022-05-22 17:07:24 +0900

    fix

--- setup-initial.sh
+++ setup-initial.sh
@@ -63,7 +63,7 @@ echo '::endgroup::'
 
 echo '::group::jenkins initialize for JAVA_OPT'
 
-JENKINS_JAVA_OPTS=$(docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' "${JENKINS_SERVICE_ID}" | grep JAVA_OPTS= | cut -d'=' -f2-)
+JENKINS_JAVA_OPTS=$(docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' "${JENKINS_SERVICE_ID}" | grep JAVA_OPTS= | cut -d'=' -f2- || :)
 for opt_set in ${JENKINS_JAVA_OPTS}; do
   OPT=$(echo "${opt_set}" | cut -d'=' -f1)
   VAL=$(echo "${opt_set}" | cut -d'=' -f2-)
