be7862d90186ba4577c79d1c67b09e99f45c391b
srz_zumix <zumix.cpp@gmail.com>
2022-06-19 17:43:01 +0900

    up

--- resources/post-action/cleanup.js
+++ resources/post-action/cleanup.js
@@ -1,13 +1,3 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-try {
-    // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-  await exec.exec('docker', ['ps', '-a']);
-} catch (error) {
-  core.setFailed(error.message);
-}
+await exec.exec('docker', ['ps', '-a']);
