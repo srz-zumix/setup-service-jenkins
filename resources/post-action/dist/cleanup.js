e04f1a3e79fecfca82b67844e58edc5f60e54fa2
srz_zumix <zumix.cpp@gmail.com>
2022-06-22 07:18:24 +0900

    fix await

--- resources/post-action/dist/cleanup.js
+++ resources/post-action/dist/cleanup.js
@@ -3448,9 +3448,9 @@ docker_ids = process.env['JENKINS_AGENT_IDS'].trim().split(' ');
 async function stop_container(docker_id) {
     try {
         console.log("Print service container logs: " + docker_id);
-        exec.exec('docker', ['logs', '--details', docker_id]);
+        await exec.exec('docker', ['logs', '--details', docker_id]);
         console.log("Stop and remove container: " + docker_id);
-        await exec.exec('docker', ['container', 'rm', '--force', docker_id]);
+        await exec.exec('docker', ['rm', '--force', docker_id]);
     } catch (error) {
         core.warning(error.message);
     }
@@ -3459,7 +3459,6 @@ async function stop_container(docker_id) {
 for (const docker_id of docker_ids) {
     stop_container(docker_id);
 }
-
 
 })();
 
--- resources/post-action/src/cleanup.js
+++ resources/post-action/src/cleanup.js
@@ -7,9 +7,9 @@ docker_ids = process.env['JENKINS_AGENT_IDS'].trim().split(' ');
 async function stop_container(docker_id) {
     try {
         console.log("Print service container logs: " + docker_id);
-        exec.exec('docker', ['logs', '--details', docker_id]);
+        await exec.exec('docker', ['logs', '--details', docker_id]);
         console.log("Stop and remove container: " + docker_id);
-        await exec.exec('docker', ['container', 'rm', '--force', docker_id]);
+        await exec.exec('docker', ['rm', '--force', docker_id]);
     } catch (error) {
         core.warning(error.message);
     }
@@ -18,4 +18,3 @@ async function stop_container(docker_id) {
 for (const docker_id of docker_ids) {
     stop_container(docker_id);
 }
-
