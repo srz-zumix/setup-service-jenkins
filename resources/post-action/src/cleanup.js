500b89af0549d8deb2880bb67ab5abf926374df5
srz_zumix <zumix.cpp@gmail.com>
2022-06-21 22:05:40 +0900

    post container stop

--- resources/post-action/src/cleanup.js
+++ resources/post-action/src/cleanup.js
@@ -1,6 +1,12 @@
 const exec = require('@actions/exec');
 const process = require('process');
 
-console.log(process.env['JENKINS_AGENT_IDS']);
+docker_ids = process.env['JENKINS_AGENT_IDS'].split(' ');
 
-exec.exec('docker', ['ps', '-a']);
+for (const docker_id of docker_ids) {
+    console.log("Print service container logs: " + docker_id)
+    exec.exec('docker', ['logs', '--details', docker_id]);
+    console.log("Stop and remove container: " + docker_id)
+    exec.exec('docker', ['rm', '--force', docker_id]);
+}
+
